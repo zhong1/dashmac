568db589212ed02ed9dad576fbe4bcde29d1a7c9
Brian <zhengbinzhong@gmail.com>
2026-04-28 08:30:24 +0000

    feat: async bootstrap in main.tsx to avoid English flash on first paint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { I18nProvider } from './i18n/index'
 import './styles/globals.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function bootstrap() {
+  const settings = await window.api.getSettings()
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <I18nProvider initialLang={settings.resolvedLanguage}>
+        <App />
+      </I18nProvider>
+    </React.StrictMode>,
+  )
+}
+
+bootstrap()
