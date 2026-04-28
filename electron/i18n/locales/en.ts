391aec90dffba0bf7060ca99d6d139d769ff766c
Brian <zhengbinzhong@gmail.com>
2026-04-28 07:09:35 +0000

    feat: add en and zh-CN main process locale dictionaries

---
+++ electron/i18n/locales/en.ts
@@ -0,0 +1,9 @@
+const en = {
+  tray: {
+    tooltip: 'DashMac',
+    open: 'Open DashMac',
+    quit: 'Quit',
+  },
+}
+
+export default en
---
+++ electron/i18n/locales/zh-CN.ts
@@ -0,0 +1,11 @@
+import en from './en'
+
+const zhCN: typeof en = {
+  tray: {
+    tooltip: 'DashMac',
+    open: '打开 DashMac',
+    quit: '退出',
+  },
+}
+
+export default zhCN
